ab8dc1787af8671595eaec8fff025dd34b4d47ab
bmhai2504 <133200286+bmhai2504@users.noreply.github.com>
2023-11-29 10:28:08 +0700

    refactor category list

--- src/components/common/Comments.js
+++ src/components/common/Comments.js
@@ -1,20 +1,15 @@
-import { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbsUp, faCommentDots } from '@fortawesome/free-solid-svg-icons'
-import authApi from "../../api/authApi/exportAuthApi";
+import React, { useEffect, useState } from "react";
 import commentApi from "../../api/comment/exportCommentApi";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import DetailMovieComment from "./DetailMovieComment";
 
 
 // const props = 1;
-const ShowComments = ({movieId}) => {
-    const [idMovie, setIdMovie] = useState(movieId);
+const ShowComments = ({ movieId }) => {
+    const [id , setId] = useState(movieId)
     const [comment, setComments] = useState();
-    const [subCommnet, setSubComment] = useState()
     const [commentList, setCommentList] = useState();
-    const [userIDTagged, setIDTagged] = useState([]);
-    const [commetID, setCommentID] = useState();
-    const [isShowTextAreaSubCommemt, setIsShowTextAreaSubCommemt] = useState(false);
+    const [isPostSuccess, setIsPostSuccess] = useState(false);
     const [isLiked, setIsLiked] = useState(false);
     const navigate = useNavigate();
     const handleLike = (e) => {
@@ -25,54 +20,30 @@ const ShowComments = ({movieId}) => {
         //     document.getElementById("like-button").style.color = "#e50914";
         // }
     }
-    const handleCommnet = (id, commentID) => {
-        setIsShowTextAreaSubCommemt(!isShowTextAreaSubCommemt);
-        setIDTagged(id);
-        setCommentID(commentID);  
-    }
     const handleChange = (e) => {
         setComments(e.target.value);
     }
-    const handleChangeSubComment = (e) => {
-        setSubComment(e.target.value);
-    }
     const fetchPostMovieComment = async (request) => {
-        return await authApi.postCommentMovie(request);
-    }
-    const fetchPostSubComment = async (request) => {
-        return await authApi.postSubCommet(request, commetID);
-    }
+        return await commentApi.postCommentMovie(request);
+  }
     const postComment = () => {
         const request = {
             comment: comment,
             datePost: new Date(),
-            movieId: movieId
+            movieId: id
         };
-        const result = fetchPostMovieComment(request);
-        console.log(result);
-        setComments("");
-    }
-    const postSubComment = () => {
-        const request = {
-            comment: subCommnet,
-            datePost: new Date(),
-            userID: userIDTagged
-        };
-        const result = fetchPostSubComment(request);
-        setSubComment("");
+        const result = fetchPostMovieComment(request).then(() => {
+            setComments("");
+            setIsPostSuccess(!isPostSuccess);
+        })
     }
     const fetchAllComments = async () => {
-        let result = await commentApi.getAllCommentsByMovieId(idMovie);
+        let result = await commentApi.getAllCommentsByMovieId(id);
         setCommentList(result?.data);
     }
-    // useEffect(() => {
-    //     if(idMovie != null){
-    //         fetchAllComments();
-    //     }
-    // })
     useEffect(() => {
-        console.log("aaa", idMovie);
-    })
+        fetchAllComments();
+    }, [isPostSuccess, id])
     return (
         <>
             <section>
@@ -96,85 +67,7 @@ const ShowComments = ({movieId}) => {
                                     <div className="row" style={{ marginTop: 20 }}>
                                         <div className="col">
                                             {commentList?.map((comment, index) => (
-                                                <div className="d-flex flex-start" key={index} style={{ marginTop: 19 }}>
-                                                    <img
-                                                        className="rounded-circle shadow-1-strong me-3" style={{ height: 60, width: 60, marginRight: 10, marginTop: -3 }}
-                                                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp"
-                                                        alt="avatar"
-                                                        width={65}
-                                                        height={65}
-                                                    />
-                                                    <div className="flex-grow-1 flex-shrink-1">
-                                                        <div>
-                                                            <div className="d-flex justify-content-between align-items-center">
-                                                                <p className="mb-1">
-                                                                    {comment?.userDto.name}{" "}
-                                                                    <span className="small">- {comment?.datePost}</span>
-                                                                </p>
-                                                            </div>
-                                                            <p className="small mb-0">
-                                                                {comment?.comment}
-                                                            </p>
-                                                            <div className="small d-flex justify-content-start" style={{ marginTop: 10 }}>
-                                                                <div className="d-flex align-items-center me-3" id="like-button" onClick={handleLike}>
-                                                                    <FontAwesomeIcon icon={faThumbsUp} size="lg" style={{ height: 25 }} />  
-                                                                </div>
-                                                                <div className="d-flex align-items-center me-3" id="comment-button" onClick={() => handleCommnet(comment?.userDto.id, comment?.id)}>
-                                                                    <FontAwesomeIcon icon={faCommentDots} size="lg" style={{ height: 25, marginLeft: 20 }} />
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        {comment?.subCommentDtoList.map((subComment, index) => (
-                                                            <div className="d-flex flex-start mt-4" key={index}>
-                                                                <Link className="me-3">
-                                                                    <img
-                                                                        className="rounded-circle shadow-1-strong"
-                                                                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(11).webp" style={{ height: 50, width: 50, marginRight: 10 }}
-                                                                        alt="avatar"
-                                                                        width={65}
-                                                                        height={65}
-                                                                    />
-                                                                </Link>
-                                                                <div className="flex-grow-1 flex-shrink-1">
-                                                                    <div>
-                                                                        <div className="d-flex justify-content-between align-items-center">
-                                                                            <p className="mb-1">
-                                                                                {subComment?.userDtoComment.name}{" "}
-                                                                                <span className="small">- 3 hours ago</span>
-                                                                            </p>
-                                                                        </div>
-                                                                        <p className="small mb-0">
-                                                                            {subComment?.comment}
-                                                                        </p>
-                                                                    </div>
-                                                                    <div className="small d-flex justify-content-start" style={{ marginTop: 10 }}>
-                                                                        <div className="d-flex align-items-center me-3" id="like-button" onClick={handleLike}>
-                                                                            <FontAwesomeIcon icon={faThumbsUp} size="lg" style={{ height: 25 }} />
-                                                                        </div>
-                                                                        <div className="d-flex align-items-center me-3" id="comment-button" onClick={() => handleCommnet(subComment?.userDtoComment.id, comment?.id)}>
-                                                                            <FontAwesomeIcon icon={faCommentDots} size="lg" style={{ height: 25, marginLeft: 20 }} />
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        ))}
-                                                        {isShowTextAreaSubCommemt ? <div
-                                                            className=" flex-grow-1 flex-shrink-1" style={{ marginTop: 16 }}
-                                                        >
-                                                            <div className="d-flex flex-start w-100 ">
-                                                                <div className="form-outline w-100">
-                                                                    <input type="text" className="form-control" id="textAreaExample" rows={4}
-                                                                        defaultValue={""} onChange={handleChangeSubComment} />
-                                                                </div>
-                                                            </div>
-                                                            <div className="float-end mt-2 pt-1">
-                                                                <button type="button" className="btn btn-outline-primary btn-sm" onClick={() => postSubComment()}>
-                                                                    Post
-                                                                </button>
-                                                            </div>
-                                                        </div> : ""}
-                                                    </div>
-                                                </div>
+                                                <DetailMovieComment commentDetail={comment} key={index} />
                                             ))}
                                         </div>
                                     </div>
--- src/components/common/ShowMovieList.js
+++ src/components/common/ShowMovieList.js
@@ -1,23 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import img1 from '../../assets/ui/images/upcoming/01.jpg'
-import categoryApi from "../../api/category/exportCategoryApi";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const ShowMovieList = () => {
-    const { id } = useParams();
+const ShowMovieList = ({ movieList }) => {
     const navigate = useNavigate();
-    const [list, setList] = useState();
-    const fetchMoviesByCategoryId = async () => {
-        let result = await categoryApi.getMoviesByCategoryId(id);
-        if (result == null) {
-            setList([]);
-            return;
-        }
-        setList(result);
-    }
-    useEffect(() => {
-        fetchMoviesByCategoryId();
-    }, [id]);
+    const [list, setList] = useState(movieList);
     const redirectToWathchingMoviePage = (name) => {
         let newName = name.replace(/ /g, "-");
         navigate(`/watch-movie/${newName}`)
@@ -26,107 +13,112 @@ const ShowMovieList = () => {
         let newName = name.replace(/ /g, "-");
         navigate(`/movie-detail/${newName}`)
     }
+
+    // console.log(list);
     return (
         <>
+
             <main id="main" className="site-main">
                 <div className="container-fluid">
                     <div className="iq-main-header d-flex align-items-center justify-content-between mt-5 mt-lg-0">
                         <h4 className="main-title">Movies</h4>
                     </div>
-                    <ul className=" row list-inline  mb-0 iq-rtl-direction ">
-                        {list?.map((movie, index) => (
-                            <li className="slide-item col-lg-3 mb-4" key={index}>
-                                <div className="block-images position-relative">
-                                    <div className="img-box">
-                                        <img
-                                            src={movie.image}
-                                            className="img-fluid"
-                                            alt=""
-                                            loading="lazy"
-                                        />
-                                    </div>
-                                    <div className="block-description">
-                                        <h6 className="iq-title">
-                                            <Link to={`/movie/${movie.id}`}>{movie.name}</Link>
-                                        </h6>
-                                        <div className="movie-time d-flex align-items-center my-2">
-                                            <span className="text-white">{movie.duration} minutes</span>
+                    {list != null ?
+                        <ul className=" row list-inline  mb-0 iq-rtl-direction ">
+                            {list?.map((movie, index) => (
+                                <li className="slide-item col-lg-3 mb-4" key={index}>
+                                    <div className="block-images position-relative">
+                                        <div className="img-box">
+                                            <img
+                                                src={movie.image}
+                                                className="img-fluid"
+                                                alt=""
+                                                loading="lazy"
+                                            />
                                         </div>
-                                        <div className="hover-buttons">
-                                            <Link role="button" className="btn btn-hover" onClick={(e) => {
-                                                e.preventDefault();
-                                                redirectToWathchingMoviePage(movie?.name)
-                                            }}>
-                                                <i className="fa fa-play mr-1" aria-hidden="true"></i>
-                                                Play Now
-                                            </Link>
+                                        <div className="block-description">
+                                            <h6 className="iq-title">
+                                                <Link to={`/movie/${movie.id}`}>{movie.name}</Link>
+                                            </h6>
+                                            <div className="movie-time d-flex align-items-center my-2">
+                                                <span className="text-white">{movie.duration} minutes</span>
+                                            </div>
+                                            <div className="hover-buttons">
+                                                <Link role="button" className="btn btn-hover" onClick={(e) => {
+                                                    e.preventDefault();
+                                                    redirectToWathchingMoviePage(movie?.name)
+                                                }}>
+                                                    <i className="fa fa-play mr-1" aria-hidden="true"></i>
+                                                    Play Now
+                                                </Link>
+                                            </div>
+                                            <div className="hover-buttons">
+                                                <Link role="button" className="btn btn-hover" onClick={(e) => {
+                                                    e.preventDefault();
+                                                    redirectToDetailMoviePage(movie?.name)
+                                                }}>
+                                                    <i className="fa fa-play mr-1" aria-hidden="true"></i>
+                                                    More details
+                                                </Link>
+                                            </div>
                                         </div>
-                                        <div className="hover-buttons">
-                                            <Link role="button" className="btn btn-hover" onClick={(e) => {
-                                                e.preventDefault();
-                                                redirectToDetailMoviePage(movie?.name)
-                                            }}>
-                                                <i className="fa fa-play mr-1" aria-hidden="true"></i>
-                                                More details
-                                            </Link>
-                                        </div>
-                                    </div>
-                                    <div className="block-social-info">
-                                        <ul className="list-inline p-0 m-0 music-play-lists">
-                                            <li className="share">
-                                                <span>
-                                                    <i className="ri-share-fill" />n
-                                                </span>
-                                                <div className="share-box">
-                                                    <div className="d-flex align-items-center">
-                                                        <Link
-                                                            to="https://www.facebook.com/sharer?u=https://iqonic.design/wp-themes/streamit_wp/movie/shadow/"
-                                                            target="_blank"
-                                                            rel="noopener noreferrer"
-                                                            className="share-ico"
-                                                            tabIndex={0}
-                                                        >
-                                                            <i className="ri-facebook-fill" />
-                                                        </Link>
-                                                        <Link
-                                                            to="https://twitter.com/intent/tweet?text=Currentlyreading"
-                                                            target="_blank"
-                                                            rel="noopener noreferrer"
-                                                            className="share-ico"
-                                                            tabIndex={0}
-                                                        >
-                                                            <i className="ri-twitter-fill" />
-                                                        </Link>
-                                                        <Link
-                                                            to="#"
-                                                            data-link="https://iqonic.design/wp-themes/streamit_wp/movie/shadow/"
-                                                            className="share-ico iq-copy-link"
-                                                            tabIndex={0}
-                                                        >
-                                                            <i className="ri-links-fill" />
-                                                        </Link>
+                                        <div className="block-social-info">
+                                            <ul className="list-inline p-0 m-0 music-play-lists">
+                                                <li className="share">
+                                                    <span>
+                                                        <i className="ri-share-fill" />n
+                                                    </span>
+                                                    <div className="share-box">
+                                                        <div className="d-flex align-items-center">
+                                                            <Link
+                                                                to="https://www.facebook.com/sharer?u=https://iqonic.design/wp-themes/streamit_wp/movie/shadow/"
+                                                                target="_blank"
+                                                                rel="noopener noreferrer"
+                                                                className="share-ico"
+                                                                tabIndex={0}
+                                                            >
+                                                                <i className="ri-facebook-fill" />
+                                                            </Link>
+                                                            <Link
+                                                                to="https://twitter.com/intent/tweet?text=Currentlyreading"
+                                                                target="_blank"
+                                                                rel="noopener noreferrer"
+                                                                className="share-ico"
+                                                                tabIndex={0}
+                                                            >
+                                                                <i className="ri-twitter-fill" />
+                                                            </Link>
+                                                            <Link
+                                                                to="#"
+                                                                data-link="https://iqonic.design/wp-themes/streamit_wp/movie/shadow/"
+                                                                className="share-ico iq-copy-link"
+                                                                tabIndex={0}
+                                                            >
+                                                                <i className="ri-links-fill" />
+                                                            </Link>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <span>
-                                                    <i className="ri-heart-fill" />
-                                                </span>
-                                                <span className="count-box">2+</span>
-                                            </li>
-                                            <li>
-                                                <span>
-                                                    <i className="ri-add-line" />
-                                                </span>
-                                            </li>
-                                        </ul>
+                                                </li>
+                                                <li>
+                                                    <span>
+                                                        <i className="ri-heart-fill" />
+                                                    </span>
+                                                    <span className="count-box">2+</span>
+                                                </li>
+                                                <li>
+                                                    <span>
+                                                        <i className="ri-add-line" />
+                                                    </span>
+                                                </li>
+                                            </ul>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+                                </li>
+                            ))}
+                        </ul> 
+                    : ""}
                 </div>
-            </main>
+            </main> :
         </>
     )
 }
