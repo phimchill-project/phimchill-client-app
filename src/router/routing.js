1713a23accce43dd61993c40cadad3dcee986707
bmhai2504 <133200286+bmhai2504@users.noreply.github.com>
2023-12-01 01:11:04 +0700

    fix commnet fearture

--- src/router/routing.js
+++ src/router/routing.js
@@ -13,7 +13,7 @@ import WatchMovie from "../views/ui/movie/watch-movie";
 import WatchTvSeries from "../views/ui/tvshow/watch-tvshow";
 import Error404 from "../views/ui/error/error404";
 import ShowTvseries from "../components/ListFilm/ShowTvSeries";
-import MovieDetail from '../components/common/ShowDetail';
+import MovieDetail from '../components/common/MovieDetail ';
 
 const publicRoutes = [
     { path: routes.login, component: Login, layout: null },
